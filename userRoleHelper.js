4fd52344613a9808b24a7ee9ab4c928c1e5ba07f
witcanstain <>
2025-08-14 14:31:12 +0300

    remove applicant role upon approval

--- userRoleHelper.js
+++ userRoleHelper.js
@@ -14,18 +14,20 @@ const approveUser = async (user_id, cmd_user_id) => {
         }
         console.info(`Member ${cmd_user_id} has command privilege.`);
         let promises = [];
-        let assignment_role = member.guild.roles.cache.find(role => role.id == process.env.ASSIGNMENT_CATEGORY_ROLE_ID);
-        let accolades_role = member.guild.roles.cache.find(role => role.id == process.env.ACCOLADES_CATEGORY_ROLE_ID);
-        let rank_role = member.guild.roles.cache.find(role => role.id == process.env.RANK_CATEGORY_ROLE_ID);
-        let medals_role = member.guild.roles.cache.find(role => role.id == process.env.MEDALS_CATEGORY_ROLE_ID);
-        let misc_role = member.guild.roles.cache.find(role => role.id == process.env.MISC_CATEGORY_ROLE_ID);
-        let member_role = member.guild.roles.cache.find(role => role.id == process.env.MEMBER_ROLE_ID);
-        let initiate_role = member.guild.roles.cache.find(role => role.id == process.env.INITIATE_ROLE_ID);
-        let visitor_role = member.guild.roles.cache.find(role => role.id == process.env.VISITOR_ROLE_ID);
+        const assignment_role = member.guild.roles.cache.find(role => role.id == process.env.ASSIGNMENT_CATEGORY_ROLE_ID);
+        const accolades_role = member.guild.roles.cache.find(role => role.id == process.env.ACCOLADES_CATEGORY_ROLE_ID);
+        const rank_role = member.guild.roles.cache.find(role => role.id == process.env.RANK_CATEGORY_ROLE_ID);
+        const medals_role = member.guild.roles.cache.find(role => role.id == process.env.MEDALS_CATEGORY_ROLE_ID);
+        const misc_role = member.guild.roles.cache.find(role => role.id == process.env.MISC_CATEGORY_ROLE_ID);
+        const member_role = member.guild.roles.cache.find(role => role.id == process.env.MEMBER_ROLE_ID);
+        const initiate_role = member.guild.roles.cache.find(role => role.id == process.env.INITIATE_ROLE_ID);
+        const visitor_role = member.guild.roles.cache.find(role => role.id == process.env.VISITOR_ROLE_ID);
+        const applicant_role = member.guild.roles.cache.find(role => role.id == process.env.APPLICANT_ROLE_ID);
         for (const role_to_add of [assignment_role, accolades_role, rank_role, medals_role, misc_role, member_role, initiate_role]) {
             if (role_to_add) promises.push(member.roles.add(role_to_add));
         }
         promises.push(member.roles.remove(visitor_role))
+        promises.push(member.roles.remove(applicant_role))
         const current_nickname = member.displayName;
         console.log(`current nickname: ${current_nickname}`);
         await member.setNickname(`[ORDER] ${current_nickname}`);
